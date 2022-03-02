3d4d9a925a364867c5d09123fce97e8e011b026f
Lee-hyeonkyu <leehyeonkyu@naver.com>
2022-03-02 19:28:34 +0900

    .

--- baseball.py
+++ baseball.py
@@ -5,10 +5,10 @@ the_number_of_ball = int(input())
 print("선택하신 갯수는"+ str(the_number_of_ball) +"입니다.")
 baseball = "".join(map(str,random.sample(range(1,10),the_number_of_ball)))
 print(baseball)
-strike = 0
-ball = 0
 count = 0
 while 1:
+    strike = 0
+    ball = 0
     count +=1
     print("숫자를 입력해주세요")
     my_ball = input()
@@ -22,6 +22,4 @@ while 1:
                 if(baseball[i]==my_ball[j]):
                     ball += 1
     print("{0}스트라이크 {1}볼 입니다.{2}회 입니다.".format(strike,ball,count))
-    strike = 0
-    ball = 0
 print("정답입니다. 총 {0}번 만에 맞췄습니다.".format(count))
